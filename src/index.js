da5e2c3447b64e58d70653d8490229d4b6d86506
raikaat <raikaat@gmail.com>
2017-09-17 13:41:04 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,32 @@
 module.exports = function multiply(first, second) {
-  var result = first*second;
-  return String(result); // your solution
+if (first*second<9007199254740991) {
+		return String(first*second);
+	} else {
+
+	var firstArr = first.split("").reverse();
+	var secondArr = second.split("").reverse();
+	var result = [];
+
+	for(var i = 0; i<firstArr.length; i++) {
+	for(var j = 0; j<secondArr.length; j++) {
+	var position = i+j;
+	if (result[position] === undefined) {
+	result[position] = 0;
+} 
+	result[position]+=firstArr[i]*secondArr[j];
+	
+}
+}
+	for(var k = 0; k<result.length; k++) {
+	if (result[k]>9) {
+if (result[k+1] === undefined) {
+	result[k+1] = 0;
+}
+	result[k+1] += Math.floor(result[k] / 10);
+	result[k] = result[k]%10;
+
+}
+}
+return result.reverse().join("");
+} 
 }
